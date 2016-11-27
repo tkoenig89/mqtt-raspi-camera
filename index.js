c461ff11c02efb1c30a9223a86f13803300916c8
TK <tobiaskoenig1@googlemail.com>
2016-11-27 10:47:39 +0100

    moving modules to separate folder

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 //set logging levels
-require("./logger")
+var logger = require("./modules/logger")
     .setDebug(false)
     .setLog(true)
     .setError(true);
 
-var MqttCameraRunner = require("./mqtt-camera-runner");
-var MqttUploader = require("./mqtt-upoad");
+var MqttCameraRunner = require("./modules/mqtt-camera-runner");
+var MqttUploader = require("./modules/mqtt-upoad");
 
 //load configuration
-var camera_config = require("./config.json");
-var mqtt_config = require("./mqtt-config.json");
+var camera_config = require(process.argv[2] || "./config.json");
+var mqtt_config = require(process.argv[3] || "./mqtt-config.json");
 
 //setup
 var mqttUploader = MqttUploader(mqtt_config);
 var runner = MqttCameraRunner(camera_config, mqttUploader);
 
 //start
+logger.log("starting up");
 runner.go();
